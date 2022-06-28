f8337b93435ac26b60005497d6cbb5e947ebb319
rukshan-villvay <rukshan@villvay.com>
2022-06-28 15:56:50 +0530

    separate back and front

--- villvay-backend/src/api/application/routes/application.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * application router.
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::application.application');
